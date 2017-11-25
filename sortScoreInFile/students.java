0b406d9bcec2cd4916ac9389cf4ab4d79e413d0c
dongyi-ma1121 <33863085+dongyi-ma1121@users.noreply.github.com>
2017-11-25 17:55:01 +0800

    Create students.java

---
+++ sortScoreInFile/students.java
@@ -0,0 +1,83 @@
+package cn.dy.test;
+
+public class Students implements Comparable<Students>{
+	String name;
+	int chinese,math,english;
+	int sum;//总分
+	public Students() {
+		super();
+		
+	}
+	public Students(String name, int chinese, int math, int english) {
+		super();
+		this.name = name;
+		this.chinese = chinese;
+		this.math = math;
+		this.english = english;
+		this.sum = chinese+math+english;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + sum;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Students other = (Students) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sum != other.sum)
+			return false;
+		return true;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getChinese() {
+		return chinese;
+	}
+	public void setChinese(int chinese) {
+		this.chinese = chinese;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getEnglish() {
+		return english;
+	}
+	public void setEnglish(int english) {
+		this.english = english;
+	}
+	public int getSum() {
+		return sum;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	@Override
+	public int compareTo(Students o) {
+		int temp = this.sum - o.sum;
+		return temp==0?this.name.compareTo(o.name):temp;
+	}
+	
+	
+}
